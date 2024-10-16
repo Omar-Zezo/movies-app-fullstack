20d701bb201400f93eeb4b4dc2e29f5b68c61d9b
Omar Abd Elaziz <80254076+Omar-Zezo@users.noreply.github.com>
2024-10-16 21:06:17 +0300

    update logged user

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,4 +2,4 @@ import axios from "axios"
 
 export const TMDB = axios.create({baseURL: "https://api.themoviedb.org/3/"})
 export const baseURL = axios.create({baseURL: "http://localhost:8000"})
-export const API_KEY = "2d1450a575e2dcacc1d2e19b768fdfdf"
+export const API_KEY = ""
