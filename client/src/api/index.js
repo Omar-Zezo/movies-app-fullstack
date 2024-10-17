d0ab584a325623ca1d901609494accc8d5f61a23
Omar-Zezo <omarabdelazizmohamed408@gmail.com>
2024-10-17 15:31:55 +0300

    change logged user password

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,5 @@ import axios from "axios"
 export const TMDB = axios.create({baseURL: "https://api.themoviedb.org/3/"})
 export const baseURL = axios.create({baseURL: "http://localhost:8000"})
 export const API_KEY = ""
+
+export const serverURL = "http://localhost:8000"
