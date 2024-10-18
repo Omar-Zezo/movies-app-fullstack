51e34fddba4f0e3dd23349139e753c0db51bc57f
Omar-Zezo <omarabdelazizmohamed408@gmail.com>
2024-10-18 13:59:11 +0300

    subscriptions

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,4 +3,4 @@ import axios from "axios"
 export const TMDB = axios.create({baseURL: "https://api.themoviedb.org/3/"})
 export const baseURL = axios.create({baseURL: "http://192.168.1.2:8000"})
 export const serverURL = "http://192.168.1.2:8000"
-export const API_KEY = "2d1450a575e2dcacc1d2e19b768fdfdf"
+export const API_KEY = ""
